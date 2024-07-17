4a0edb6659ffb332a4807593eaeeff420afacc05
Max Demian <iammaxdemian@gmail.com>
2024-07-16 21:38:38 -0700

    encrypt delivery address

--- scripts/store/nft.js
+++ scripts/store/nft.js
@@ -6,10 +6,18 @@ const MEH_STORE_V1 = process.env.MEH_STORE_V1;
 const MEH_STORE_NFT = process.env.MEH_STORE_NFT;
 
 
+const CryptoJS = require('crypto-js');
 
 //npx hardhat run scripts/store/nft.js --network basesepolia
 //npx hardhat run scripts/store/nft.js --network base
 async function main() {
+
+    const address = '1347 Skyline Dr, Winona MN 55987';
+    const secretKey = 'that-gum-you-like-is-coming-back-in-style';
+
+    const encrypted = CryptoJS.AES.encrypt(address, secretKey).toString();
+
+    console.log('encrypted:', encrypted);
 
 
     const MehStoreNFT = await ethers.getContractFactory("MehStoreNFT");
@@ -17,11 +25,16 @@ async function main() {
 
     await mehStoreNFT.enterDeliveryAddress(
         0,
-        "1347 Skyline Drive, Winona MN 55987"
+        encrypted
     );
 
     const details = await mehStoreNFT.nftDetails(0);
     console.log(details);
+
+    const bytes = CryptoJS.AES.decrypt(details.deliveryAddress, secretKey);
+    const decryptedString = bytes.toString(CryptoJS.enc.Utf8);
+
+    console.log(decryptedString);
 
 }
 
